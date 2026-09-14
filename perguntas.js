cab9bbea4c91e0dd1ccf8ab758c1734076b51e83
gabrielmassuda-jpg <gabriel.massuda@escola.pr.gov.br>
2026-09-14 12:05:44 -0300

    fghy

--- perguntas.js
+++ perguntas.js
@@ -1,21 +1,45 @@
 criarCartao(
-'categoria',
-'Perguntas',
-'Respostas'
+'categoria',Matematica
+'Perguntas',20+20+20+7
+'Respostas'Ai é muito facil professora 67
 
 
 )
 criarCartao(
-'categoria',
-'Perguntas',
-'Respostas'
+'categoria',Historia
+'Perguntas',Quem participou da guerra de ópio
+'Respostas'Inglaterra e china
 
 
 )
 criarCartao(
-'categoria',
-'Perguntas',
-'Respostas'
+'categoria',Artes
+'Perguntas',Grupo musical responsavel pelo movimento post punk dos anos 2000
+'Respostas'The Strokes)
+
+criarCartao(
+'categoria',Esportes
+'Perguntas',Quem ganhou a copa do brasil em 2010
+'Respostas'Santos Futebol Clube
 
 
+)
+criarCartao(
+'categoria',Fisica
+'Perguntas',Oque é inerce
+'Respostas'Propriedade física que faz com que a matéria resista a qualquer mudança em seu estado de repouso ou de movimento
+
+
+)
+criarCartao(
+'categoria',Artes
+'Perguntas',Principal pintor do movimento surrealista
+'Respostas'Salvador Dalí
+
+
+)
+criarCartao(
+'categoria',Geografia
+'Perguntas',Qual é o unico pais neutro
+'Respostas'Suíça
 )
